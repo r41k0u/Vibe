613fd4e3fa77df7cd54e44aba4185cb353515bf9
Angad Kambli <angadkambli@gmail.com>
2022-01-29 01:23:43 +0530

    add particles

--- utility_scenes/bullet.gd
+++ utility_scenes/bullet.gd
@@ -6,22 +6,27 @@ extends Node2D
 # var b = "text"
 
 var velocity = Vector2(0, 0)
+var hit = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_entered", self, "_on_Area2D_body_entered")
 	pass # Replace with function body.
 
 func _process(delta):
-	position.x += velocity.x*delta
-	position.y += velocity.y*delta
+	if(!hit):
+		position.x += velocity.x*delta
+		position.y += velocity.y*delta
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
 func _on_Area2D_body_entered(body):
-	print("reached")
 	if body.get_parent().has_method("on_hit"):
 		body.get_parent().on_hit()
+		hit = true
+		$Impact.emitting = true
+		$Bullet.visible = false
+		yield(get_tree().create_timer(3.0), "timeout")
 		queue_free()
 	pass # Replace with function body.
