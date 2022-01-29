540a65c716c2832b0bb431bd247d0503cc3db71f
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2022-01-29 21:34:32 +0530

    HUD fix

--- HUD.gd
+++ HUD.gd
@@ -9,13 +9,13 @@ var window_size
 var items_to_center = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	items_to_center.append($SCORE)
-	items_to_center.append($TIMER)
 	window_size = get_viewport().size
 	for item in items_to_center:
 		item.position.x = window_size.x/2
 		item.position.y = window_size.y/2
-	pass # Replace with function body.
+	$SCORE.position = Vector2(512, 302)
+	$TIMER.position = Vector2(512, 302)
+pass # Replace with function body.
 
 func _process(delta):
 	var new_window_size = get_viewport().size
