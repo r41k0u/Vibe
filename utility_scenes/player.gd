644d51df67b5e050ea044e3c578222489ab31ae2
Angad Kambli <angadkambli@gmail.com>
2022-01-29 14:41:14 +0530

    change bullet

--- utility_scenes/player.gd
+++ utility_scenes/player.gd
@@ -6,7 +6,7 @@ extends Node2D
 # var b = "text"
 export var MAX_ORBIT_VELOCITY = 2
 export var ORBITAL_ACCEL = 0.05
-export var BULLET_COOLDOWN = 0.1
+export var BULLET_COOLDOWN = 0.05
 var bullet_time = 0
 var orbit_velocity = 0
 var bullet = preload("res://utility_scenes/bullet.tscn")
@@ -35,6 +35,7 @@ func _process(delta):
 		bullet_time = BULLET_COOLDOWN
 		var b = bullet.instance()
 		b.position = $Player_motif.global_position
+		b.rotation = rotation
 		b.velocity = (global_position - $Player_motif.global_position)*2.5
 		get_parent().add_child(b)
 	else:
